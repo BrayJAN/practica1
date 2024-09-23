f987d7be2adb4c738267a85fb5cfa1a60178ea74
BrayJAN <braynanj123@gmail.com>
2024-09-22 20:37:59 -0600

    first commit

---
+++ src/main/java/crodriguez/practica1/Controller/HomeController.java
@@ -0,0 +1,14 @@
+package crodriguez.practica1.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
+public class HomeController {
+    @GetMapping("/")
+    public String home(){
+        return"Hola mundo!";
+    }
+} 
+
---
+++ src/main/java/crodriguez/practica1/Controller/Practica1Application.java
@@ -0,0 +1,13 @@
+package crodriguez.practica1.Controller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Practica1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Practica1Application.class, args);
+	}
+
+}
